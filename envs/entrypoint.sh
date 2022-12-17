a962559a8c1c41fa249ab9c9e6a84da635efba2d
David Duarte <davidandradeduarte@gmail.com>
2022-12-17 19:31:52 +0000

    fix: docker only run $SHELL if exit code is 0

--- envs/entrypoint.sh
+++ envs/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install dotfiles
 /bin/bash <(curl -fsSL https://raw.githubusercontent.com/davidandradeduarte/dot/HEAD/install.sh) $1
 
-# Run default shell
-$SHELL
+if [ $? -eq 0 ]; then
+    $SHELL
+fi
