796cdefabd0c23eb6c2896ff90c7a63b6f981c03
David Duarte <davidandradeduarte@gmail.com>
2022-12-20 02:43:59 +0000

    fix: remove comment

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -10,6 +10,9 @@ export PATH=$PATH:/usr/local/go/bin
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
+# TODO: on macOS we need this
+# eval $(/opt/homebrew/bin/brew shellenv)
+
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
--- installers/golang/ubuntu/install.sh
+++ installers/golang/ubuntu/install.sh
@@ -4,7 +4,6 @@ install_golang_ubuntu() {
     set_error_trap
     echo_inf "Installing ${GREEN}golang${NO_COLOR} for Ubuntu..."
 
-    # if arch is arm download arm version
     if [[ "$(uname -m)" == "aarch64" ]]; then
         curl -OL https://go.dev/dl/go1.19.4.linux-arm64.tar.gz
         sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.4.linux-arm64.tar.gz
@@ -14,9 +13,4 @@ install_golang_ubuntu() {
         sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
         rm go1.19.4.linux-amd64.tar.gz
     fi
-
-    # TODO: this needs to be added to the .bashrc provided
-    # export PATH="$(go env GOPATH)/bin:$PATH"
-    # find a way to make it generic across OS
-    export PATH=$PATH:/usr/local/go/bin
 }
